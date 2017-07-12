2ce5143e29d10a112318be5ab01b8444c8a442ec
Anwar HIdayat <aan.elzam93@gmail.com>
2017-07-12 09:42:28 +0700

    tester

---
+++ testing/server-no-red.js
@@ -0,0 +1,32 @@
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+// var redis = require('socket.io-redis');
+// io.adapter(redis({ host: 'localhost', port: 6379 }));
+var numConnected = 0;
+
+app.listen(3000);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+        numConnected++;
+        console.log('client connected ('+numConnected+').');
+  socket.emit('halo', { hello: 'world '+numConnected });
+        // socket.on('disconnect', function(){
+        //         numConnected--;
+        //         console.log('client disconnected ('+numConnected+').')
+        // });
+});
+
---
+++ testing/test.js
@@ -0,0 +1,22 @@
+        
+
+// for(var i = 0;i<5000;i++){
+setInterval(function() {
+
+        var numConnected = 0;
+        var socket = require('socket.io-client')('http://localhost:3000');
+        socket.on('connect', function(){
+                numConnected++;
+                console.log('client connected');
+        });
+        socket.on('halo', function(data){
+                console.log(data);
+        });
+        socket.on('disconnect', function(){
+                numConnected--;
+                console.log('client disconnected.');
+        });
+},100)
+
+// }
+
